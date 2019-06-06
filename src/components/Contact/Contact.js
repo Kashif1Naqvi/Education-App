81789f33e073204b30106a103c2e746e107c37cc
Kashif1Naqvi <naqvishah582@gmail.com>
2019-06-06 20:59:56 +0500

    set app

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
       <div className="container text-muted"  style={{marginTop:"80px"}} >
             <div id="wrapper">
                 <section>
-                   <img src="https://scontent.flhe3-1.fna.fbcdn.net/v/t1.0-9/15940528_373004099728938_7550667385047692076_n.jpg?_nc_cat=101&_nc_ht=scontent.flhe3-1.fna&oh=6791ae42485ca813f07a60f719751d98&oe=5CB04F9A" alt="Photograph of Kashif naqvi" className="profile-photo" />
+                   <img src="https://scontent.fisb6-1.fna.fbcdn.net/v/t1.0-9/15940528_373004099728938_7550667385047692076_n.jpg?_nc_cat=101&_nc_ht=scontent.fisb6-1.fna&oh=41d56051aab8fcfd7b2cbf1d221fad1d&oe=5D9D9D9A" alt="Photograph of Kashif naqvi" className="profile-photo" />
                     <h3>About</h3>
                       <p>i'm Front end web Developer at Digital Cru.my CEO is <a href="https://github.com/murtza"><b><i>Sir  MURTZA</i></b></a>,who tell me Different ways of learning Programming and understand Technical Logics</p>
                       <p>Hi, I'm Kashif! This is my design portfolio where I share all of my favroite work. When I'm not designing things, When i am free then i'm Learning Data science,Data WareHouse and more.</p>
